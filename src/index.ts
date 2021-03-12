65489c7fec3c2fa32fe9a8845c456b2c8d60c3f2
Tim_Tan <forsoapfc@gmail.com>
2021-03-12 16:10:40 +0800

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,42 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer } from "apollo-server";
+import { context } from "./context";
+import schema from "./schema";
 
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
-
-const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Query {
-    books: [Book]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ context, schema });
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
+
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// async function main() {
+//   // ... you will write your Prisma Client queries here
+
+//   // await prisma.user.create({
+//   //   data: {
+//   //     name: "Tan",
+//   //     email: "tan@gmail.io",
+//   //     posts: {
+//   //       create: { title: "Hello World" },
+//   //     },
+//   //     profile: {
+//   //       create: { bio: "This is my bio" },
+//   //     },
+//   //   },
+//   // });
+
+//   const allUsers = await prisma.post.findMany();
+//   console.log(allUsers);
+// }
+
+// main()
+//   .catch((e) => {
+//     throw e;
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
